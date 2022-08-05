64ab0b6266a6c38079f1c76a44903fc1ad771d43
thisago <thiago@oxyoy.com>
2022-08-05 11:53:50 +0000

    Updated docs

--- src/util/forData.nim
+++ src/util/forData.nim
@@ -1,13 +1,14 @@
 ## Utilities `forData` manipulation and analysis
-from std/tables import Table, `[]`, `[]=`, hasKey, pairs
+# from std/tables import Table, `[]`, `[]=`, hasKey, pairs
+import std/tables
 
 proc occurrences*[T](xs: openArray[T]): Table[T, int] =
   ## Get all occurrences of values in array
   runnableExamples:
-    from std/tables import toTable
+    import std/tables
     doAssert occurrences(["a", "b", "c", "a"]) == {"a": 2, "b": 1, "c": 1}.toTable
     doAssert occurrences(["a", "b", "c", "c"]) == {"a": 1, "b": 1, "c": 2}.toTable
-    doAssert occurrences(["a", "a", "b", "c", "c"]) == {"a": 1, "b": 1, "c": 2}.toTable
+    doAssert occurrences(["a", "a", "b", "c", "c"]) == {"a": 2, "b": 1, "c": 2}.toTable
   for x in xs:
     result[x] =
       if not result.hasKey x:
@@ -18,7 +19,7 @@ proc occurrences*[T](xs: openArray[T]): Table[T, int] =
 proc occurrence*[T](xs: openArray[T], val: T): int =
   ## Get the occurrence of specific value in array
   runnableExamples:
-    from std/tables import toTable
+    import std/tables
     doAssert(["a", "b", "c", "a"].occurrence("b") == 1)
     doAssert(["a", "b", "c", "c"].occurrence("e") == 0)
     doAssert(["a", "a", "b", "c", "c"].occurrence("c") == 2)
@@ -29,10 +30,10 @@ proc occurrence*[T](xs: openArray[T], val: T): int =
 proc mostCommon*[T](xs: openArray[T]): seq[T] =
   ## Get the most common values at array
   runnableExamples:
-    from std/tables import toTable
+    import std/tables
     doAssert mostCommon(["a", "b", "c", "a"]) == ["a"]
     doAssert mostCommon(["a", "b", "c", "c"]) == ["c"]
-    doAssert mostCommon(["a", "a", "b", "c", "c"]) == ["a", "c"]
+    doAssert mostCommon(["a", "a", "b", "c", "c"]) == ["c", "a"]
   let xsOccurrences = xs.occurrences
   var maxOccurrence = 0
   for val, occurrence in xsOccurrences:
